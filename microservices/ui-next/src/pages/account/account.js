f1a9d80382a3778653c9ac79707bd9b3972a6287
Moussa <harajli98@gmail.com>
2019-05-01 14:17:12 -0400

    Refactoring Part 2: Fixed layout issues

--- microservices/ui-next/src/pages/account/account.js
+++ microservices/ui-next/src/pages/account/account.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import MainNavigator from "layout/MainNavigator/MainNavigator";
 import HeroLayout from "layout/HeroLayout/HeroLayout";
-
 import Steak from "assets/image/steak.png";
-import {Grid, Typography} from "@material-ui/core";
+import {Typography} from "@material-ui/core";
 import Box from "@material-ui/core/Box";
+import MainNavigatorSection from "layout/MainNavigator/MainNavigatorSection";
 
 const Account = () => {
     return (
         <MainNavigator>
-            <Grid item xs={12}>
+            <MainNavigatorSection>
                 <HeroLayout variant="large" image={Steak}>
                     <Box flexGrow={1}>
                         <Typography variant="h2" align="center">My Account</Typography>
                     </Box>
                 </HeroLayout>
-            </Grid>
+            </MainNavigatorSection>
         </MainNavigator>
     );
 };
--- microservices/ui-next/src/pages/contact/contact.js
+++ microservices/ui-next/src/pages/contact/contact.js
@@ -2,19 +2,20 @@ import React from 'react';
 import HeroLayout from "layout/HeroLayout/HeroLayout";
 import Steak from "assets/image/steak.png";
 import Box from "@material-ui/core/Box";
-import {Grid, Typography} from "@material-ui/core";
+import {Typography} from "@material-ui/core";
 import MainNavigator from "layout/MainNavigator/MainNavigator";
+import MainNavigatorSection from "layout/MainNavigator/MainNavigatorSection";
 
 const Contact = () => {
     return (
         <MainNavigator>
-            <Grid item xs={12}>
+            <MainNavigatorSection>
                 <HeroLayout variant="large" image={Steak}>
                     <Box flexGrow={1}>
                         <Typography variant="h2" align="center">Contact Us</Typography>
                     </Box>
                 </HeroLayout>
-            </Grid>
+            </MainNavigatorSection>
         </MainNavigator>
     );
 };

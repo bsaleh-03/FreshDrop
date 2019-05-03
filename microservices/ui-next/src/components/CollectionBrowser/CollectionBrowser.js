d75d25a265d554c391e45f906f5a7f02579c44f4
Moussa <harajli98@gmail.com>
2019-05-03 14:06:59 -0400

    Refactoring Part 2: Added loading indicator to products page

--- microservices/ui-next/src/components/CollectionBrowser/CollectionBrowser.js
+++ microservices/ui-next/src/components/CollectionBrowser/CollectionBrowser.js
@@ -1,14 +1,14 @@
 import React, {useEffect} from 'react';
 import PropTypes from 'prop-types';
 import Styles from "./Styles";
-import {Divider, Grid, Typography, withStyles} from "@material-ui/core";
+import {CircularProgress, Divider, Grid, Typography, withStyles} from "@material-ui/core";
 import ProductCard from "components/ProductCard/ProductCard";
 import {bindActionCreators} from "redux";
 import {fetchCollections} from "redux/actions";
 import {connect} from "react-redux";
 import Box from "@material-ui/core/Box";
 
-const CollectionBrowser = ({ classes, collection, collections, fetchCollections }) => {
+const CollectionBrowser = ({ classes, collection, collections, loading, fetchCollections }) => {
 
     // Fetch collections from shopify
     useEffect(() => {
@@ -20,24 +20,30 @@ const CollectionBrowser = ({ classes, collection, collections, fetchCollections 
     }
 
     return (
-        <React.Fragment>
-            { collections.map((collection, idx) => (
-                <Box mb={2} flexGrow={1}>
-                    <Box mb={2}>
-                        <Typography variant="h5" gutterBottom>{collection.title}</Typography>
-                        <Divider component="hr" />
-                    </Box>
+        loading === true ? (
+            <Box display="flex" flexGrow={1} flexDirection="row" justifyContent="center">
+                <CircularProgress />
+            </Box>
+        ) : (
+            <React.Fragment>
+                { collections.map((collection, idx) => (
+                    <Box mb={2} flexGrow={1}>
+                        <Box mb={2}>
+                            <Typography variant="h5" gutterBottom>{collection.title}</Typography>
+                            <Divider component="hr" />
+                        </Box>
 
-                    <Grid container spacing={2} key={idx}>
-                        { collection.products.map((product, idx) => (
-                            <Grid item xs={12} sm={6} md={3} lg={2} key={idx} style={{display: "flex"}}>
-                                <ProductCard product={product} />
-                            </Grid>
-                        ))}
-                    </Grid>
-                </Box>
-            ))}
-        </React.Fragment>
+                        <Grid container spacing={2} key={idx}>
+                            { collection.products.map((product, idx) => (
+                                <Grid item xs={12} sm={6} md={3} lg={2} key={idx} style={{display: "flex"}}>
+                                    <ProductCard product={product} />
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Box>
+                ))}
+            </React.Fragment>
+        )
     );
 };
 
@@ -48,7 +54,8 @@ const mapDispatchToProps = dispatch => {
 };
 
 const mapStateToProps = state => ({
-   collections: state.collections.items
+    loading: state.collections.loading,
+    collections: state.collections.items
 });
 
 CollectionBrowser.propTypes = {
